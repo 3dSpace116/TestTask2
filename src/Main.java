e627a9d0ddc751ce85cbf2156ff6f8a6bc506815
3dSpace116 <mirskam@gmail.com>
2024-12-16 23:00:51 +0300

    решено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+//Напишите пример обработки нескольких исключений в одном блоке catch.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int[] nums = {1, 2, 3};
+        int[] nullArray = null;
+try{
+        System.out.println(nums[3]+nullArray[0]);}
+catch(ArrayIndexOutOfBoundsException|NullPointerException e){
+    System.out.println("Error");
+}
+
     }
 }
